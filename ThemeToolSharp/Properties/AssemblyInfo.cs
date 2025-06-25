f382c2a180b22519bbe768b89d2cb83463d36538
namazso <admin@namazso.eu>
2025-06-25 04:50:14 +0200

    Bump version

--- ThemeToolSharp/Properties/AssemblyInfo.cs
+++ ThemeToolSharp/Properties/AssemblyInfo.cs
@@ -23,11 +23,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ThemeToolSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Windows theme changer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("namazso")]
 [assembly: AssemblyProduct("ThemeToolSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyCopyright("Copyright © 2025  namazso <admin@namazso.eu>")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
